6cfe4282eb61c62bc242be943d42caa757d048b6
BPV Systems <4629370+benvince@users.noreply.github.com>
2022-12-03 22:35:05 +0000

    Add day2_2 test data & code tidy

--- solutions/day2.py
+++ solutions/day2.py
@@ -1,6 +1,8 @@
 
 SHAPE_POINTS = {"A": 1, "B": 2, "C": 3, "X": 1, "Y":2, "Z":3}
 SHAPE_DEFEATS = {"A": "Z", "B":"X", "C": "Y"}
+SHAPE_LOSES = {"A": "B", "B":"C", "C": "A"}
+SHAPE_WINS =  {v: k for k, v in SHAPE_LOSES.items()}
 
 SOLUTION_DATA = 'data/day2'
 
@@ -26,5 +28,22 @@ def day_2_1():
 
     return score
 
+def day_2_2():
+
+    score = 0
+    for x in get_data():
+        if x[1] == "X":
+            # Lose
+            score += SHAPE_POINTS[SHAPE_WINS[x[0]]]
+        elif x[1] == "Y":
+            # Draw:
+            score += SHAPE_POINTS[x[0]] + 3
+        else:
+            # Win
+            score += SHAPE_POINTS[SHAPE_LOSES[x[0]]] + 6
+
+    return score
+
 
 print(f"Solution 1 Answer: {day_2_1()}.")
+print(f"Solution 2 Answer: {day_2_2()}.")
